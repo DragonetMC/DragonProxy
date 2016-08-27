5ccbcfca9ce2e748eb3fb9f986de1d1bfaa045fe
Рауль Вахитов <mrgenga537@gmail.com>
2016-08-27 12:16:01 +0500

    Fix PEDownstream Session doesn't receive reply of DataPacket

--- proxy/src/main/java/org/dragonet/proxy/network/PEDownstreamSession.java
+++ proxy/src/main/java/org/dragonet/proxy/network/PEDownstreamSession.java
@@ -186,7 +186,7 @@ public class PEDownstreamSession implements DownstreamSession<PEPacket>, ClientI
         }
 
         EncapsulatedPacket encapsulated = new EncapsulatedPacket();
-        encapsulated.buffer = Binary.appendBytes((byte) 0xfe, packet.getData());
+        encapsulated.buffer = packet.getData();
         encapsulated.needACK = true;
         encapsulated.reliability = (byte) 2;
         encapsulated.messageIndex = 0;
