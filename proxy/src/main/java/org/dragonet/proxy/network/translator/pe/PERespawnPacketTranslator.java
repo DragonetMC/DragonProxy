d80596348ed937a18acd4e7ede4bc1f74716c572
robotman3000 <robotman3000@icloud.com>
2017-01-05 20:25:21 -0500

    Another missing translator

---
+++ proxy/src/main/java/org/dragonet/proxy/network/translator/pe/PERespawnPacketTranslator.java
@@ -0,0 +1,20 @@
+package org.dragonet.proxy.network.translator.pe;
+
+import org.dragonet.proxy.network.UpstreamSession;
+import org.dragonet.proxy.network.translator.PEPacketTranslator;
+import org.spacehq.mc.protocol.data.game.ClientRequest;
+import org.spacehq.mc.protocol.packet.ingame.client.ClientRequestPacket;
+import org.spacehq.packetlib.packet.Packet;
+
+import cn.nukkit.network.protocol.RespawnPacket;
+
+public class PERespawnPacketTranslator implements PEPacketTranslator<RespawnPacket> {
+
+	@Override
+	public Packet[] translate(UpstreamSession session, RespawnPacket packet) {
+		System.err.println("Respawning Client");
+		ClientRequestPacket pack = new ClientRequestPacket(ClientRequest.RESPAWN);
+		return new Packet[]{pack};
+	}
+
+}
