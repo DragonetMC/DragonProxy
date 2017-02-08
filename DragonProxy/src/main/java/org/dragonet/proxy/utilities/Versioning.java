d93ef8e1f6875aaa27ea76c6888069c4cb62b6af
robotman3000 <robotman3000@icloud.com>
2017-02-07 21:30:22 -0500

    Updated versioning

--- DragonProxy/src/main/java/org/dragonet/proxy/utilities/Versioning.java
+++ DragonProxy/src/main/java/org/dragonet/proxy/utilities/Versioning.java
@@ -16,14 +16,14 @@ import org.spacehq.mc.protocol.MinecraftConstants;
 
 public class Versioning {
 
-    public final static int VERSION_INT = 00601; // 0.0.6.01 (You can't have preR1 in an int!)
-    public final static String RELEASE_VERSION = "0.0.6-preR1";
+    public final static int VERSION_INT = 007; //History: 0.0.6.01 (You can't have preR1 in an int!)
+    public final static String RELEASE_VERSION = "0.0.7";
 
     public final static String MINECRAFT_PC_VERSION = MinecraftConstants.GAME_VERSION;
     public final static int MINECRAFT_PC_PROTOCOL = MinecraftConstants.PROTOCOL_VERSION;
 
     // This is STRICT to MCPE binary's definition, DO NOT CHANGE
-    public final static String MINECRAFT_PE_VERSION = "1.0.0";
+    public final static String MINECRAFT_PE_VERSION = "1.0.2";
     
     public final static int MINECRAFT_PE_PROTOCOL = 100;
 
