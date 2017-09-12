aba5a89a064ebc212a6a64faf6de82e26d83bb4d
DefinitlyEvil <theevildays@gmail.com>
2017-09-12 16:34:51 +0800

    update versioning

--- proxy/src/main/java/org/dragonet/proxy/utilities/Versioning.java
+++ proxy/src/main/java/org/dragonet/proxy/utilities/Versioning.java
@@ -14,15 +14,14 @@ package org.dragonet.proxy.utilities;
 
 public class Versioning {
 
-    public final static int VERSION_INT = 00601; // 0.0.6.01 (You can't have preR1 in an int!)
-    public final static String RELEASE_VERSION = "0.0.6-preR1";
+    public final static String RELEASE_VERSION = "0.1.0-preR1";
 
-    public final static String MINECRAFT_PC_VERSION = "1.8.9";
-    public final static int MINECRAFT_PC_PROTOCOL = 47;
+    public final static String MINECRAFT_PC_VERSION = "1.12.1";
+    // public final static int MINECRAFT_PC_PROTOCOL = 47;
 
     // This is STRICT to MCPE binary's definition, DO NOT CHANGE
-    public final static String MINECRAFT_PE_VERSION = "0.16.1";
+    public final static String MINECRAFT_PE_VERSION = "1.1.4";
     
-    public final static int MINECRAFT_PE_PROTOCOL = 91;
+    public final static int MINECRAFT_PE_PROTOCOL = 113;
 
 }
