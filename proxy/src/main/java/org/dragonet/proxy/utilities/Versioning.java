85e88db7ea54eb4284b215514ea365c3d8cb5c7f
DefinitlyEvil <theevildays@gmail.com>
2017-11-26 13:44:32 +0800

    release for 0.2.1-preR1

--- proxy/src/main/java/org/dragonet/proxy/utilities/Versioning.java
+++ proxy/src/main/java/org/dragonet/proxy/utilities/Versioning.java
@@ -16,8 +16,8 @@ import org.dragonet.proxy.protocol.ProtocolInfo;
 
 public class Versioning {
 	// vars
-	public static final String RELEASE_VERSION = "0.2.0-preR2";
-	public static final String MINECRAFT_PC_VERSION = "1.12.1";
+	public static final String RELEASE_VERSION = "0.2.1-preR1";
+	public static final String MINECRAFT_PC_VERSION = "1.12.2";
 	// public static final int MINECRAFT_PC_PROTOCOL = 47;
 	// This is STRICT to MCPE binary's definition, DO NOT CHANGE
 	public static final String MINECRAFT_PE_VERSION = ProtocolInfo.MINECRAFT_VERSION;
