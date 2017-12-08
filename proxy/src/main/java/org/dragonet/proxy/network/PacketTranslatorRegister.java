cc0563e8867efffa7bb0d3543d3a1cc151da9669
Vincent <pcsrvadm@gmail.com>
2017-12-08 17:52:38 +0100

    add new packet translator for entity rotation

--- proxy/src/main/java/org/dragonet/proxy/network/PacketTranslatorRegister.java
+++ proxy/src/main/java/org/dragonet/proxy/network/PacketTranslatorRegister.java
@@ -74,6 +74,7 @@ public final class PacketTranslatorRegister {
 		PC_TO_PE_TRANSLATOR.put(ServerEntityDestroyPacket.class, new PCDestroyEntitiesPacketTranslator());
 		PC_TO_PE_TRANSLATOR.put(ServerEntityPositionRotationPacket.class,new PCEntityPositionRotationPacketTranslator());
 		PC_TO_PE_TRANSLATOR.put(ServerEntityPositionPacket.class, new PCEntityPositionPacketTranslator());
+		PC_TO_PE_TRANSLATOR.put(ServerEntityRotationPacket.class, new PCEntityRotationPacketTranslator());
 		PC_TO_PE_TRANSLATOR.put(ServerEntityVelocityPacket.class, new PCEntityVelocityPacketTranslator());
 		PC_TO_PE_TRANSLATOR.put(ServerEntityEffectPacket.class, new PCEntityEffectPacketTranslator());
 		PC_TO_PE_TRANSLATOR.put(ServerEntityRemoveEffectPacket.class, new PCEntityRemoveEffectPacketTranslator());
