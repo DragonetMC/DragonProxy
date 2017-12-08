5a3218a7f0d9d8b0c2db07caadf752d79c28cdc8
Vincent <pcsrvadm@gmail.com>
2017-12-08 17:53:56 +0100

    add default attributes to mob packet translator

--- proxy/src/main/java/org/dragonet/proxy/network/translator/pc/PCSpawnMobPacketTranslator.java
+++ proxy/src/main/java/org/dragonet/proxy/network/translator/pc/PCSpawnMobPacketTranslator.java
@@ -17,6 +17,7 @@ import org.dragonet.proxy.network.UpstreamSession;
 import org.dragonet.proxy.network.cache.CachedEntity;
 import org.dragonet.proxy.network.translator.IPCPacketTranslator;
 import com.github.steveice10.mc.protocol.packet.ingame.server.entity.spawn.ServerSpawnMobPacket;
+import org.dragonet.proxy.entity.PEEntityAttribute;
 import org.dragonet.proxy.protocol.PEPacket;
 import org.dragonet.proxy.protocol.packets.AddEntityPacket;
 import org.dragonet.proxy.utilities.Vector3F;
@@ -36,17 +37,18 @@ public class PCSpawnMobPacketTranslator implements IPCPacketTranslator<ServerSpa
 			if (e == null) {
 				return null;
 			}
+                        
+                            AddEntityPacket pk = new AddEntityPacket();
+                            pk.rtid = e.proxyEid;
+                            pk.eid = e.proxyEid;
+                            pk.type = e.peType.getPeType();
+                            pk.position = new Vector3F((float) e.x, (float) e.y, (float) e.z);
+                            pk.motion = new Vector3F((float) e.motionX, (float) e.motionY, (float) e.motionZ);
+                            // TODO: Hack for now. ;P
+                            pk.meta = EntityMetaData.createDefault();
+                            pk.attributes = new PEEntityAttribute[]{};
 
-			AddEntityPacket pk = new AddEntityPacket();
-			pk.rtid = e.proxyEid;
-			pk.eid = e.proxyEid;
-			pk.type = e.peType.getPeType();
-			pk.position = new Vector3F((float) e.x, (float) e.y, (float) e.z);
-			pk.motion = new Vector3F((float) e.motionX, (float) e.motionY, (float) e.motionZ);
-			// TODO: Hack for now. ;P
-			pk.meta = EntityMetaData.createDefault();
-
-			return new PEPacket[] { pk };
+                            return new PEPacket[] { pk };
 		} catch (Exception e) {
 			e.printStackTrace();
 			return null;
