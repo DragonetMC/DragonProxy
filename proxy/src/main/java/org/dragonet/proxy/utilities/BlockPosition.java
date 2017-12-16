00245b405fc7a613458ac71bf8a4f48cbeca9118
HoverEpic <pcsrvadm@gmail.com>
2017-12-16 19:09:29 +0100

    add equals() method to BlockPosition

--- proxy/src/main/java/org/dragonet/proxy/utilities/BlockPosition.java
+++ proxy/src/main/java/org/dragonet/proxy/utilities/BlockPosition.java
@@ -4,24 +4,29 @@ package org.dragonet.proxy.utilities;
  * Created on 2017/10/21.
  */
 public class BlockPosition {
-	// vars
-	public int x;
-	public int y;
-	public int z;
 
-	// constructor
-	public BlockPosition() {
+    public int x;
+    public int y;
+    public int z;
 
-	}
+    public BlockPosition() {
 
-	public BlockPosition(int x, int y, int z) {
-		this.x = x;
-		this.y = y;
-		this.z = z;
-	}
+    }
 
-	// public
+    public BlockPosition(int x, int y, int z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
 
-	// private
+    @Override
+    public boolean equals(Object obj) {
+        if (!(obj instanceof BlockPosition)) {
+            return false;
+        }
 
+        BlockPosition other = (BlockPosition) obj;
+
+        return this.x == other.x && this.y == other.y && this.z == other.z;
+    }
 }
