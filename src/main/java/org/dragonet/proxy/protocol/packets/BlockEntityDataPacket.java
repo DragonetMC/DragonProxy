aee30654811186d9918359abe8de4997540d846b
HoverEpic <pcsrvadm@gmail.com>
2017-12-23 14:34:58 +0100

    Reformat and fix Block entity packet

--- src/main/java/org/dragonet/proxy/protocol/packets/BlockEntityDataPacket.java
+++ src/main/java/org/dragonet/proxy/protocol/packets/BlockEntityDataPacket.java
@@ -4,52 +4,49 @@ import org.dragonet.proxy.protocol.PEPacket;
 import org.dragonet.proxy.protocol.ProtocolInfo;
 import org.dragonet.proxy.utilities.BlockPosition;
 
-import com.github.steveice10.opennbt.NBTIO;
-import com.github.steveice10.opennbt.tag.builtin.CompoundTag;
-
 import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
+import java.nio.ByteOrder;
+import org.dragonet.proxy.data.nbt.NBTIO;
+import org.dragonet.proxy.data.nbt.tag.CompoundTag;
 
 /**
  * Created on 2017/10/22.
  */
 public class BlockEntityDataPacket extends PEPacket {
-	//vars
-	public BlockPosition blockPosition;
-	public CompoundTag tag;
-	
-	//constructor
-	public BlockEntityDataPacket() {
-		
-	}
-	
-	//public
-	public int pid() {
-		return ProtocolInfo.BLOCK_ENTITY_DATA_PACKET;
-	}
-	
-	public void encodePayload() {
-		putBlockPosition(blockPosition);
-		if (tag != null) {
-			ByteArrayOutputStream bos = new ByteArrayOutputStream();
-			try {
-				NBTIO.writeTag(bos, tag);
-				bos.close();
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-			put(bos.toByteArray());
-		}
-	}
-	public void decodePayload() {
-		blockPosition = getBlockPosition();
-		try {
-			tag = (CompoundTag) NBTIO.readTag(new ByteArrayInputStream(get(getBuffer().length - offset)));
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-	
-	//private
-	
+
+    public BlockPosition blockPosition;
+    public CompoundTag tag;
+
+    public BlockEntityDataPacket() {
+
+    }
+
+    @Override
+    public int pid() {
+        return ProtocolInfo.BLOCK_ENTITY_DATA_PACKET;
+    }
+
+    @Override
+    public void encodePayload() {
+        putBlockPosition(blockPosition);
+        if (tag != null) {
+            byte[] bytes = new byte[]{};
+            try {
+                bytes = NBTIO.write(tag, ByteOrder.LITTLE_ENDIAN);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            put(bytes);
+        }
+    }
+
+    @Override
+    public void decodePayload() {
+        blockPosition = getBlockPosition();
+        try {
+            tag = (CompoundTag) NBTIO.read(new ByteArrayInputStream(get(getBuffer().length - offset)), ByteOrder.LITTLE_ENDIAN);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
