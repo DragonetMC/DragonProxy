f0b7ad3369fb7406a9f5d77b237dd9f695b86fa3
Niek Vincent <apocalypsjenl@gmail.com>
2018-01-02 19:31:49 +0100

    Spacing fix?

--- src/main/java/org/dragonet/proxy/network/translator/itemsblocks/SlabDataTranslator.java
+++ src/main/java/org/dragonet/proxy/network/translator/itemsblocks/SlabDataTranslator.java
@@ -3,14 +3,12 @@ package org.dragonet.proxy.network.translator.itemsblocks;
 public class SlabDataTranslator extends IItemDataTranslator {
 
 	@Override
-	public Integer translateToPC(Integer damage) {
-		System.out.println(damage);
+    public Integer translateToPC(Integer damage) {
 		return damage;
 	}
 
 	@Override
 	public Integer translateToPE(Integer damage) {
-		System.out.println(damage);
 		if(damage == 6) return 7;
 		if(damage == 7) return 6;
 		if(damage == 14) return 15;
