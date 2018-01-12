bfef58f4ae4b97068437f70df42152548c911902
HoverEpic <pcsrvadm@gmail.com>
2018-01-12 14:28:06 +0100

    Add metrics system,

--- proxy/src/main/java/org/dragonet/proxy/configuration/PropertiesConfig.java
+++ proxy/src/main/java/org/dragonet/proxy/configuration/PropertiesConfig.java
@@ -14,22 +14,26 @@ package org.dragonet.proxy.configuration;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.nio.charset.Charset;
 import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class PropertiesConfig {
 
+    private final File file;
     private final Properties config;
 
     public PropertiesConfig(String defaultResourcePath, String fileName, boolean saveDefault) throws IOException {
         Properties defaultConfig = new Properties();
         defaultConfig.load(PropertiesConfig.class.getResourceAsStream(defaultResourcePath));
         config = new Properties(defaultConfig);
-        File file = new File(fileName);
+        file = new File(fileName);
         if (file.exists()) {
             FileInputStream input = new FileInputStream(file);
             config.load(new InputStreamReader(input, Charset.forName("UTF-8")));
@@ -37,11 +41,24 @@ public class PropertiesConfig {
             FileOutputStream fos = new FileOutputStream(fileName);
             InputStream ris = PropertiesConfig.class.getResourceAsStream(defaultResourcePath);
             int d = -1;
-            while ((d = ris.read()) != -1) {
+            while ((d = ris.read()) != -1)
                 fos.write(d);
-            }
             fos.close();
             ris.close();
+        }
+    }
+
+    public void save() throws IOException {
+        FileOutputStream out = null;
+        try {
+            out = new FileOutputStream(file);
+            config.store(out, "");
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(PropertiesConfig.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(PropertiesConfig.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            out.close();
         }
     }
 
