d54688af326a8e5edfb645030c44dde6c67ad9b5
HoverEpic <pcsrvadm@gmail.com>
2018-02-22 09:31:22 +0100

    Add try/catch CLSAutheticationService

--- proxy/src/main/java/org/dragonet/proxy/utilities/CLSAuthenticationService.java
+++ proxy/src/main/java/org/dragonet/proxy/utilities/CLSAuthenticationService.java
@@ -14,7 +14,7 @@ package org.dragonet.proxy.utilities;
 
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
-import java.util.Collections;
+import com.google.gson.JsonSyntaxException;
 import java.util.HashMap;
 import java.util.Map;
 import org.dragonet.proxy.network.UpstreamSession;
@@ -42,17 +42,16 @@ public class CLSAuthenticationService {
     }
 
     public boolean authenticate(UpstreamSession session) {
-        if (session.getProfile().extraData != null)
-            if (session.getProfile().extraData.has("XUID") && session.getProfile().extraData.has("identity") && session.getProfile().extraData.has("displayName")) {
-                Map<String, String> postData = new HashMap();
-                postData.put("XUID", session.getProfile().extraData.get("XUID").getAsString());
-                postData.put("identity", session.getProfile().extraData.get("identity").getAsString());
-                postData.put("displayName", session.getProfile().extraData.get("displayName").getAsString());
-                String rawResult = HTTP.performPostRequest(this.clsServer + "/api/v1/authenticate", postData);
-                if (rawResult != null) {
+        if (session.getProfile().isLoginVerified()) {
+            Map<String, String> postData = new HashMap();
+            postData.put("chain", session.getProfile().getChainJWT());
+            postData.put("token", session.getProfile().getClientDataJWT());
+            String rawResult = HTTP.performPostRequest(this.clsServer + "/api/v1/authenticate", postData);
+            if (rawResult != null)
+                try {
                     JsonObject result = gson.fromJson(rawResult, JsonObject.class);
-                     if (result.has("pin")) {
-                        session.getDataCache().put("cls_link_server", this.clsServer + "/register.html");
+                    if (result.has("pin")) {
+                        session.getDataCache().put("cls_link_server", this.clsServer + "/link");
                         session.getDataCache().put("cls_link_pin", result.get("pin").getAsString());
                         return false;
                     } else if (result.has("success")) {
@@ -62,9 +61,16 @@ public class CLSAuthenticationService {
                         session.getDataCache().put("mojang_displayName", result.get("displayName").getAsString());
                         return true;
                     }
+                } catch (JsonSyntaxException ex) {
+                    DragonProxy.getInstance().getLogger().warning("An error occured on CLS server.");
+                    return false;
                 }
+            else {
+                DragonProxy.getInstance().getLogger().warning("Unable to connect to CLS server.");
+                return false;
             }
-        session.sendChat("Something bad happened, report the problem !");
+        }
+        DragonProxy.getInstance().getLogger().warning("Trying to authenticate " + session.getUsername() + " with CLS server but Xbox Live not connected.");
         return false;
     }
 
@@ -74,11 +80,18 @@ public class CLSAuthenticationService {
             postData.put("uuid", (String) session.getDataCache().get("mojang_uuid"));
             postData.put("accessToken", newAccessToken);
             String rawResult = HTTP.performPostRequest(this.clsServer + "/api/v1/refresh", postData);
-            if (rawResult != null) {
-                JsonObject result = gson.fromJson(rawResult, JsonObject.class);
-                if (result.has("success")) {
-                    return true;
+            if (rawResult != null)
+                try {
+                    JsonObject result = gson.fromJson(rawResult, JsonObject.class);
+                    if (result.has("success"))
+                        return true;
+                } catch (JsonSyntaxException ex) {
+                    DragonProxy.getInstance().getLogger().warning("An error occured on CLS server.");
+                    return false;
                 }
+            else {
+                DragonProxy.getInstance().getLogger().warning("Unable to connect to CLS server.");
+                return false;
             }
         }
         return false;
