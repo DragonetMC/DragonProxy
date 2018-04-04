69618809b700d007d8b52eed8f0c749cf0379151
Misat11 <misa.turek1@gmail.com>
2018-04-04 17:28:39 +0200

    fix

--- proxy/src/main/java/org/dragonet/proxy/network/translator/pc/PCBlockChangePacketTranslator.java
+++ proxy/src/main/java/org/dragonet/proxy/network/translator/pc/PCBlockChangePacketTranslator.java
@@ -19,9 +19,6 @@ import com.github.steveice10.mc.protocol.packet.ingame.server.world.ServerBlockC
 import org.dragonet.proxy.network.UpstreamSession;
 import org.dragonet.proxy.network.translator.IPCPacketTranslator;
 import org.dragonet.proxy.network.translator.ItemBlockTranslator;
-import org.dragonet.proxy.network.translator.SoundTranslator;
-
-import java.nio.ByteBuffer;
 
 import org.dragonet.common.data.blocks.GlobalBlockPalette;
 import org.dragonet.common.data.itemsblocks.ItemEntry;
