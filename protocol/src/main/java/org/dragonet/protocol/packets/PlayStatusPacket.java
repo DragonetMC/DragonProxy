0d174dba464e859ee820b61cfd53c4f599287355
SupremeMortal <SupremeMortal@users.noreply.github.com>
2018-05-21 21:37:50 +0100

    1.4.0 support. Fixes #323 (#324)
    * Bump protocol to 261 (1.4.0)
    
    * Protocol changes
    
    * Minified runtime ID table for 1.4.0
    Somehow I forgot this.

--- protocol/src/main/java/org/dragonet/protocol/packets/PlayStatusPacket.java
+++ protocol/src/main/java/org/dragonet/protocol/packets/PlayStatusPacket.java
@@ -15,6 +15,7 @@ public class PlayStatusPacket extends PEPacket {
     public static final int LOGIN_FAILED_INVALID_TENANT = 4;
     public static final int LOGIN_FAILED_VANILLA_EDU = 5;
     public static final int LOGIN_FAILED_EDU_VANILLA = 6;
+    public static final int LOGIN_FAILED_SERVER_FULL = 7;
 
     public int status;
     public int protocol = ProtocolInfo.CURRENT_PROTOCOL;
