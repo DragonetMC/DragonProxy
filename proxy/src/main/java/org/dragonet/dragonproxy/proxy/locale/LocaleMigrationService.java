42bdccfc84a870687f05b6179aed65ab8e356b43
Gabriele C <sgdc3.mail@gmail.com>
2018-10-11 14:02:54 +0200

    License headers

--- proxy/src/main/java/org/dragonet/dragonproxy/proxy/locale/LocaleMigrationService.java
+++ proxy/src/main/java/org/dragonet/dragonproxy/proxy/locale/LocaleMigrationService.java
@@ -1,3 +1,16 @@
+/*
+ * GNU LESSER GENERAL PUBLIC LICENSE
+ * Version 3, 29 June 2007
+ *
+ * Copyright (C) 2007 Free Software Foundation, Inc. <http://fsf.org/>
+ * Everyone is permitted to copy and distribute verbatim copies
+ * of this license document, but changing it is not allowed.
+ *
+ * You can view the LICENCE file for details.
+ *
+ * @author Dragonet Foundation
+ * @link https://github.com/DragonetMC/DragonProxy
+ */
 package org.dragonet.dragonproxy.proxy.locale;
 
 import ch.jalu.configme.configurationdata.ConfigurationData;
