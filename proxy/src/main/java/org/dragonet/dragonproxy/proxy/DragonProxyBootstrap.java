6bae58ff29198a494831fc1045d9db0c68dd657f
EOT3000 <43685885+EOT3000@users.noreply.github.com>
2019-01-24 18:03:54 -0500

    Change some stuff for better usability

--- proxy/src/main/java/org/dragonet/dragonproxy/proxy/DragonProxyBootstrap.java
+++ proxy/src/main/java/org/dragonet/dragonproxy/proxy/DragonProxyBootstrap.java
@@ -46,8 +46,8 @@ public class DragonProxyBootstrap {
             logger.info("Version: " + DragonProxyBootstrap.class.getPackage().getImplementationVersion());
             return;
         }
-        int bedrockPort = options.has(bedrockPortOption) ? Integer.parseInt(options.valueOf(bedrockPortOption)) : -1;
-        int javaPort = options.has(javaPortOption) ? Integer.parseInt(options.valueOf(bedrockPortOption)) : -1;
+        int bedrockPort = options.has(bedrockPortOption) ? Integer.parseInt(options.valueOf(bedrockPortOption)) : 19132;
+        int javaPort = options.has(javaPortOption) ? Integer.parseInt(options.valueOf(bedrockPortOption)) : 25565;
 
         long startTime = System.currentTimeMillis();
         DragonProxy proxy = new DragonProxy(bedrockPort, javaPort);
