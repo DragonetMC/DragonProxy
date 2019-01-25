aa69db90acc7ee0513d7303db393f3bae69021ee
EOT3000 <43685885+EOT3000@users.noreply.github.com>
2019-01-24 20:07:16 -0500

    Add packet reader and bind to port

---
+++ proxy/src/main/java/org/dragonet/dragonproxy/proxy/server/ServerInit.java
@@ -0,0 +1,22 @@
+package org.dragonet.dragonproxy.proxy.server;
+
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.handler.codec.DelimiterBasedFrameDecoder;
+import io.netty.handler.codec.Delimiters;
+import io.netty.handler.codec.string.StringDecoder;
+import io.netty.handler.codec.string.StringEncoder;
+
+public class ServerInit extends ChannelInitializer<SocketChannel> {
+    @Override
+    protected void initChannel(SocketChannel channel) {
+        ChannelPipeline pipeline = channel.pipeline();
+
+        pipeline.addLast("framer", new DelimiterBasedFrameDecoder(8192, Delimiters.lineDelimiter()));
+        pipeline.addLast("decoder", new StringDecoder());
+        pipeline.addLast("encoder", new StringEncoder());
+
+        pipeline.addLast("handler", new ServerHandler());
+    }
+}
