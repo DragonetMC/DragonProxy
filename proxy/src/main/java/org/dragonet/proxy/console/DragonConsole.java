8e15b302e632c400c7e2e00cf44fbffabad88666
SupremeMortal <suprememortal@users.noreply.github.com>
2019-08-04 16:52:57 +0100

    Fix inverted return value.

--- proxy/src/main/java/org/dragonet/proxy/console/DragonConsole.java
+++ proxy/src/main/java/org/dragonet/proxy/console/DragonConsole.java
@@ -45,7 +45,7 @@ public class DragonConsole extends SimpleTerminalConsole {
 
     @Override
     protected boolean isRunning() {
-        return !proxy.isRunning();
+        return proxy.isRunning();
     }
 
     @Override
