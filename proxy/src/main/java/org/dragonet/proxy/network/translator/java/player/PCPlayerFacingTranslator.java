96b116426cbb82a2cb35e7adfae1c66f45de66f7
Luke <luke.wyatt21@gmail.com>
2020-04-09 14:40:01 +0100

    Remove test code

--- proxy/src/main/java/org/dragonet/proxy/network/translator/java/player/PCPlayerFacingTranslator.java
+++ proxy/src/main/java/org/dragonet/proxy/network/translator/java/player/PCPlayerFacingTranslator.java
@@ -21,30 +21,10 @@ public class PCPlayerFacingTranslator extends PacketTranslator<ServerPlayerFacin
 
         CachedEntity targetEntity = session.getEntityCache().getByRemoteId(packet.getTargetEntityId());
         if(targetEntity == null) {
-            log.warn("Target entity is null");
+//            log.warn("Target entity is null");
             return;
         }
 
-        double d0 = player.getPosition().getX() - targetEntity.getPosition().getX();
-        double d1 = player.getPosition().getY() - targetEntity.getPosition().getY();
-        double d2 = player.getPosition().getZ() - targetEntity.getPosition().getZ();
-        double d3 = (double)Math.sqrt(d0 * d0 + d2 * d2);
-        double rotationPitch = wrapDegrees((float)(-(Math.atan2(d1, d3) * (double)(180F / (float)Math.PI))));
-        double rotationYaw = wrapDegrees((float)(Math.atan2(d2, d0) * (double)(180F / (float)Math.PI)) - 90.0F);
-
         //player.rotate(session, Vector3f.from(rotationPitch, rotationYaw, rotationYaw));
-    }
-
-    public static double wrapDegrees(double value) {
-        value = value % 360.0D;
-        if (value >= 180.0D) {
-            value -= 360.0D;
-        }
-
-        if (value < -180.0D) {
-            value += 360.0D;
-        }
-
-        return value;
     }
 }
