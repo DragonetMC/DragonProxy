809b64708d508f5a40cc365503331f8468bab714
James Harrison <james@fasttortoise.co.uk>
2020-04-20 15:14:22 +0100

    Added subMOTD to replace world name as on LAN games Removed MOTD from Ping pass through as java MOTD look completely broken on bedrock Ping passthrough now only managed player count

--- proxy/src/main/java/org/dragonet/proxy/network/session/ProxySession.java
+++ proxy/src/main/java/org/dragonet/proxy/network/session/ProxySession.java
@@ -405,7 +405,7 @@ public class ProxySession implements PlayerSession {
         startGamePacket.setWorldTemplateOptionLocked(false);
 
         startGamePacket.setLevelId(UUID.randomUUID().toString());
-        startGamePacket.setWorldName("DragonProxy " + proxy.getVersion());
+        startGamePacket.setWorldName(proxy.getConfiguration().getMotd2());
         startGamePacket.setPremiumWorldTemplateId("00000000-0000-0000-0000-000000000000");
         startGamePacket.setCurrentTick(0);
         startGamePacket.setEnchantmentSeed(0);
