7084eca3ec0893226aaf90b6009c42a276fc7ad0
James Harrison <james@fasttortoise.co.uk>
2020-05-01 01:18:58 +0100

    Fix crash due to end_gateway Fix crash due to custom tab list

---
+++ proxy/src/main/java/org/dragonet/proxy/network/translator/misc/tile/EndGatewayEntityTranslator.java
@@ -0,0 +1,27 @@
+package org.dragonet.proxy.network.translator.misc.tile;
+
+import com.github.steveice10.opennbt.tag.builtin.CompoundTag;
+import com.nukkitx.nbt.CompoundTagBuilder;
+import com.nukkitx.nbt.tag.IntTag;
+import com.nukkitx.nbt.tag.ListTag;
+import lombok.extern.log4j.Log4j2;
+import org.dragonet.proxy.util.registry.BlockEntityRegisterInfo;
+
+import java.sql.Array;
+import java.util.Arrays;
+import java.util.List;
+
+@Log4j2
+@BlockEntityRegisterInfo(bedrockId = "EndGateway")
+public class EndGatewayEntityTranslator implements IBlockEntityTranslator {
+
+    @Override
+    public void translateToBedrock(CompoundTagBuilder builder, CompoundTag javaTag) {
+        builder.intTag("Age", Long.valueOf(javaTag.get("Age").getValue().toString()).intValue());
+        builder.byteTag("ExactLocation", (byte) 1);
+
+        // Values not needed as java manages it all
+        List<IntTag> exitPortal = Arrays.asList(new IntTag("x", 0), new IntTag("y", 0), new IntTag("z", 0));
+        builder.tag(new ListTag("ExitPortal", IntTag.class, exitPortal));
+    }
+}
