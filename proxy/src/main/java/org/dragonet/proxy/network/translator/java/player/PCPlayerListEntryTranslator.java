f0ca19fda0d86d1073f9a5b47199970a29896e5a
James Harrison <james@fasttortoise.co.uk>
2020-05-02 01:46:06 +0100

    PR didn't merge so re-pushing

--- proxy/src/main/java/org/dragonet/proxy/network/translator/java/player/PCPlayerListEntryTranslator.java
+++ proxy/src/main/java/org/dragonet/proxy/network/translator/java/player/PCPlayerListEntryTranslator.java
@@ -42,8 +42,6 @@ public class PCPlayerListEntryTranslator extends PacketTranslator<ServerPlayerLi
 
         for(PlayerListEntry entry : packet.getEntries()) {
 
-            if(entry.getDisplayName() == null || entry.getDisplayName().toString().isEmpty()) { return; }
-
             PlayerListPacket.Entry bedrockEntry = new PlayerListPacket.Entry(entry.getProfile().getId());
             String displayName = entry.getProfile().getName();
             //Check player name is a valid minecraft name
